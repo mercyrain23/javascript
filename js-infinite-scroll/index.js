7319e5b5cfcbb78571d8375ea356eda820a3c639
이선우 <mercyrain23@gmail.com>
2021-02-25 22:16:07 +0900

    IntersectionObserver

--- js-infinite-scroll/index.js
+++ js-infinite-scroll/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 import renderList from './listRenderer';
-import { debounce } from './util';
+// import { debounce } from "./util";
 
 const app = document.querySelector('#app');
 const fetchMoreTrigger = document.querySelector('#fetchMore');
@@ -13,14 +13,25 @@ const fetchMore = async () => {
 	target.classList.remove('loading');
 };
 
-const onScroll = e => {
-	// do something (hint: e.target.scrollingElement)
-	console.dir(e.target.scrollingElement);
-	const { scrollHeight, scrollTop, clientHeight } = e.target.scrollingElement;
-	if (scrollTop + clientHeight === scrollHeight) {
-		fetchMore();
-	}
-};
+// const onScroll = e => {
+//   // do something (hint: e.target.scrollingElement)
+//   console.dir(e.target.scrollingElement);
+//   const { scrollHeight, scrollTop, clientHeight } = e.target.scrollingElement;
+//   console.log(scrollTop);
+//   if (scrollTop + clientHeight === scrollHeight) {
+//     fetchMore();
+//   }
+// };
 
-document.addEventListener('scroll', debounce(onScroll, 300));
+// document.addEventListener("scroll", debounce(onScroll, 300));
+// fetchMore();
+
+const fetchMoreObserver = new IntersectionObserver(([{ isIntersecting }]) => {
+	// console.log(entry);
+	//isIntersecting: false
+	//isVisible: false
+	if (isIntersecting) fetchMore();
+});
+fetchMoreObserver.observe(fetchMoreTrigger);
+
 fetchMore();
