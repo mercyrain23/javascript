7e9df043b368f40a9d97c6b31f793540e85f9a78
이선우 <mercyrain23@gmail.com>
2021-02-25 21:58:12 +0900

    debounce

--- js-infinite-scroll/index.js
+++ js-infinite-scroll/index.js
@@ -1,25 +1,26 @@
-import "./style.css";
-import renderList from "./listRenderer";
+import './style.css';
+import renderList from './listRenderer';
+import { debounce } from './util';
 
-const app = document.querySelector("#app");
-const fetchMoreTrigger = document.querySelector("#fetchMore");
+const app = document.querySelector('#app');
+const fetchMoreTrigger = document.querySelector('#fetchMore');
 let page = 0;
 
 const fetchMore = async () => {
-  const target = page ? fetchMoreTrigger : app;
-  target.classList.add("loading");
-  await renderList(page++);
-  target.classList.remove("loading");
+	const target = page ? fetchMoreTrigger : app;
+	target.classList.add('loading');
+	await renderList(page++);
+	target.classList.remove('loading');
 };
 
 const onScroll = e => {
-  // do something (hint: e.target.scrollingElement)
-  console.dir(e.target.scrollingElement);
-  const { scrollHeight, scrollTop, clientHeight } = e.target.scrollingElement;
-  if (scrollTop + clientHeight === scrollHeight) {
-    fetchMore();
-  }
+	// do something (hint: e.target.scrollingElement)
+	console.dir(e.target.scrollingElement);
+	const { scrollHeight, scrollTop, clientHeight } = e.target.scrollingElement;
+	if (scrollTop + clientHeight === scrollHeight) {
+		fetchMore();
+	}
 };
 
-document.addEventListener("scroll", onScroll);
+document.addEventListener('scroll', debounce(onScroll, 300));
 fetchMore();
--- js-infinite-scroll/util.js
+++ js-infinite-scroll/util.js
@@ -1,8 +1,15 @@
 const getRandomSeconds = () => (Math.round(Math.random() * 5) + 1) * 250;
 
 export const randomTimer = (func, ...args) => resolve => {
-  setTimeout(() => resolve(func(...args)), getRandomSeconds());
+	setTimeout(() => resolve(func(...args)), getRandomSeconds());
 };
 
-export const dummyFetcher = (method, args) =>
-  new Promise(randomTimer(method, args));
+export const dummyFetcher = (method, args) => new Promise(randomTimer(method, args));
+
+export const debounce = (func, delay) => {
+	let timeoutId = null;
+	return (...arg) => {
+		clearTimeout(timeoutId);
+		timeoutId = setTimeout(func.bind(null, ...arg), delay);
+	};
+};
